444905e8487d65f8f04fe9c5f64bfe7a8e462105
unknown <spyynova@gmail.com>
2024-01-14 18:27:31 +0330

    create buttons and slider

--- lib/screens/song_screen.dart
+++ lib/screens/song_screen.dart
@@ -25,12 +25,17 @@ class _SongScreenState extends State<SongScreen> {
   @override
   void initState() {
     super.initState();
-    ConcatenatingAudioSource(children: []);
     audioPlayer.setAudioSource(
       ConcatenatingAudioSource(
         children: [
           AudioSource.uri(
-            Uri.parse('assets:///${song.url}'),
+            Uri.parse('asset:///${song.url}'),
+          ),
+          AudioSource.uri(
+            Uri.parse('asset:///${Song.songs[1].url}'),
+          ),
+          AudioSource.uri(
+            Uri.parse('asset:///${Song.songs[2].url}'),
           ),
         ],
       ),
@@ -49,17 +54,8 @@ class _SongScreenState extends State<SongScreen> {
             fit: BoxFit.cover,
           ),
           BackDropGradient(),
-          StreamBuilder<SeekBarData>(
-            stream: _seekBarDataStream,
-            builder: (context, snapshot) {
-              final positionData = snapshot.data;
-              return SeekBar(
-                duration: positionData?.duration ?? Duration.zero,
-                position: positionData?.duration ?? Duration.zero,
-                onChanged: audioPlayer.seek,
-              );
-            },
-          )
+          MusicPlayer(
+              seekBarDataStream: _seekBarDataStream, audioPlayer: audioPlayer),
         ],
       ),
     );
@@ -99,5 +95,114 @@ class _SongScreenState extends State<SongScreen> {
   void dispose() {
     audioPlayer.dispose();
     super.dispose();
+  }
+}
+
+class MusicPlayer extends StatelessWidget {
+  const MusicPlayer({
+    super.key,
+    required Stream<SeekBarData> seekBarDataStream,
+    required this.audioPlayer,
+  }) : _seekBarDataStream = seekBarDataStream;
+
+  final Stream<SeekBarData> _seekBarDataStream;
+  final AudioPlayer audioPlayer;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 20, vertical: 40),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          StreamBuilder<SeekBarData>(
+            stream: _seekBarDataStream,
+            builder: (context, snapshot) {
+              final positionData = snapshot.data;
+              return SeekBar(
+                duration: positionData?.duration ?? Duration.zero,
+                position: positionData?.position ?? Duration.zero,
+                onChanged: audioPlayer.seek,
+              );
+            },
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              StreamBuilder<SequenceState?>(
+                stream: audioPlayer.sequenceStateStream,
+                builder: (context, index) {
+                  return IconButton(
+                    onPressed: audioPlayer.hasPrevious
+                        ? audioPlayer.seekToPrevious
+                        : null,
+                    icon: Icon(Icons.skip_previous),
+                    color: Colors.white,
+                    iconSize: 45,
+                  );
+                },
+              ),
+              StreamBuilder(
+                stream: audioPlayer.playerStateStream,
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    final playerState = snapshot.data;
+                    final processingState =
+                        (playerState! as PlayerState).processingState;
+                    if (processingState == ProcessingState.loading ||
+                        processingState == ProcessingState.buffering) {
+                      return Container(
+                        width: 64,
+                        height: 64,
+                        margin: EdgeInsets.all(10),
+                        child: CircularProgressIndicator(),
+                      );
+                    } else if (!audioPlayer.playing) {
+                      return IconButton(
+                        iconSize: 65,
+                        onPressed: audioPlayer.play,
+                        color: Colors.white,
+                        icon: Icon(Icons.play_circle),
+                      );
+                    } else if (processingState != ProcessingState.completed) {
+                      return IconButton(
+                        iconSize: 65,
+                        onPressed: audioPlayer.pause,
+                        color: Colors.white,
+                        icon: Icon(Icons.pause_circle),
+                      );
+                    } else {
+                      return IconButton(
+                        iconSize: 65,
+                        onPressed: () => audioPlayer.seek(
+                          Duration.zero,
+                          index: audioPlayer.effectiveIndices!.first,
+                        ),
+                        icon: Icon(Icons.replay_circle_filled),
+                        color: Colors.white,
+                      );
+                    }
+                  } else {
+                    return Container();
+                  }
+                },
+              ),
+              StreamBuilder<SequenceState?>(
+                stream: audioPlayer.sequenceStateStream,
+                builder: (context, index) {
+                  return IconButton(
+                    onPressed:
+                        audioPlayer.hasNext ? audioPlayer.seekToNext : null,
+                    icon: Icon(Icons.skip_next),
+                    color: Colors.white,
+                    iconSize: 45,
+                  );
+                },
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
--- lib/widget/seek_bar.dart
+++ lib/widget/seek_bar.dart
@@ -1,7 +1,6 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 
 class SeekBarData {
   final Duration position;
@@ -10,7 +9,7 @@ class SeekBarData {
   SeekBarData({required this.position, required this.duration});
 }
 
-class SeekBar extends StatelessWidget {
+class SeekBar extends StatefulWidget {
   const SeekBar({
     super.key,
     required this.duration,
@@ -22,32 +21,68 @@ class SeekBar extends StatelessWidget {
   final Duration duration;
   final ValueChanged<Duration>? onChanged;
   final ValueChanged<Duration>? onChangeEnd;
+
+  @override
+  State<SeekBar> createState() => _SeekBarState();
+}
+
+class _SeekBarState extends State<SeekBar> {
+  String formatDuration(Duration duration) {
+    if (duration == null) {
+      return '--:--';
+    } else {
+      String minutes = duration.inMinutes.toString().padLeft(2, '0');
+      String seconds =
+          duration.inSeconds.remainder(60).toString().padLeft(2, '0');
+      return '${minutes}:${seconds}';
+    }
+  }
+
+  double? dragValue;
   @override
   Widget build(BuildContext context) {
-    double? dragValue;
     return Row(
       children: [
+        Text(formatDuration(widget.duration)),
         Expanded(
           child: SliderTheme(
             data: SliderTheme.of(context).copyWith(
               trackHeight: 4,
               thumbShape: RoundSliderThumbShape(
-                enabledThumbRadius: 4,
-                disabledThumbRadius: 4,
+                enabledThumbRadius: 7,
+                disabledThumbRadius: 7,
               ),
               overlayShape: RoundSliderOverlayShape(overlayRadius: 0),
               activeTrackColor: Colors.white,
-              inactiveTrackColor: Colors.white,
+              inactiveTrackColor: Colors.grey,
               thumbColor: Colors.white,
               overlayColor: Colors.white,
             ),
             child: Slider(
-              value: min(dragValue ?? widget.position.inMilliseconds.toDouble(),
-                  widget.position.inMilliseconds.toDouble()),
-              onChanged: (value) {},
+              min: 0,
+              max: widget.duration.inMilliseconds.toDouble(),
+              value: min(
+                dragValue ?? widget.position.inMilliseconds.toDouble(),
+                widget.duration.inMilliseconds.toDouble(),
+              ),
+              onChanged: (value) {
+                setState(() {
+                  dragValue = value;
+                });
+                if (widget.onChanged != null) {
+                  widget.onChanged!(Duration(milliseconds: value.round()));
+                }
+              },
+              onChangeEnd: (value) {
+                if (widget.onChangeEnd != null) {
+                  widget.onChangeEnd!(Duration(milliseconds: value.round()));
+                }
+                dragValue = null;
+              },
             ),
           ),
         ),
+        Text(formatDuration(widget.position)),
       ],
     );
   }
